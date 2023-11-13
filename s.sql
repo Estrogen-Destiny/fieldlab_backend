409635d2e97f9cab45e17cbd02af9dc5725ab6a4
David Ngo <164350@student.horizoncollege.nl>
2023-11-13 12:17:00 +0100

    database added

---
+++ s.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS `stayNL`;
+
+CREATE DATABASE `stayNL`;
+
+USE `stayNL`;
+
+CREATE TABLE `staynl`.`kamer_info` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` TEXT NOT NULL,
+    `location` TEXT NOT NULL,
+    `foto` TEXT NOT NULL,
+    `informatie_kamer` TEXT NOT NULL,
+    `faciliteiten` TEXT NOT NULL,
+    `beschikbaarheid` DATE NOT NULL,
+    `kamer_spec` TEXT NOT NULL,
+    PRIMARY KEY (`id`)
+);
