0d3a948a638d41fa25adf2cb54df09fcfb3b7c2a
Marai <163207@student.horizoncollege.nl>
2023-12-11 10:41:21 +0100

    sql

--- reservation.sql
+++ reservation.sql
@@ -7,7 +7,7 @@ USE `stayNL`;
 CREATE TABLE `reservation` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `property id` integer NOT NULL,
-    `guest name` integer NOT NULL,
+    `guest name` varchar(40) NOT NULL,
     `check in date` date NOT NULL,
     `check out date` date NOT NULL,
     PRIMARY KEY (`id`)
